ac7e46433c1e5329777a37d75c93df4c91d191ec
DHipo <104331457+DHipo@users.noreply.github.com>
2025-03-16 14:16:42 -0300

    Initial commit

---
+++ include/Entity/Entity.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "../Utils.h"
+
+class Entity
+{
+public:
+	Entity(SDL_Rect, const char *);
+	~Entity();
+
+	virtual void Draw(SDL_Renderer*);
+	virtual void Update(const Utils::TimeInter&);
+	
+	SDL_FRect* getPointerOfRect() { return &m_rect; };
+	void setPosition();
+
+protected:
+	SDL_FRect m_rect;
+	SDL_Surface * m_image;
+};
+
